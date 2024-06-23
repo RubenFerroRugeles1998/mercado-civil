127c08e85f6a9bd9bc974360449b23455fd6868f
Ruben Ferro <ruben.ferro@aspsols.com>
2024-06-23 11:05:04 -0500

    Landing page component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.scss';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LandingPage/>
   );
 }
 
 export default App;
+
---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './LandingPage.module.scss';
+
+const LandingPage = () => {
+  return (
+    <div>
+        Landing page works
+    </div>
+  )
+}
+
+export default LandingPage;
