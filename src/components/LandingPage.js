a474af930850b030b2285f4f5065851001331865
Ruben Ferro <ruben.ferro@aspsols.com>
2024-06-23 21:55:37 -0500

    week 2 update

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from './LandingPage.module.scss';
 import { Each } from './Each';
+import products from '../data/products.json';
 import logo from "../assets/Images/LandingPage/logoMercadoCivil.png";
 import casco from "../assets/Images/constructionProducts/casco.jpeg";
 import pala from "../assets/Images/constructionProducts/pala.jpg";
@@ -16,6 +17,16 @@ const LandingPage = () => {
         { name: "Pica", image: pica, price: 185000 },
         { name: "Carretilla", image: carretilla, price: 585000 },
         { name: "Overol", image: overol, price: 58000 },
+        { name: "Casco", image: casco, price: 85000 },
+        { name: "Pala", image: pala, price: 75000 },
+        { name: "Pica", image: pica, price: 185000 },
+        { name: "Carretilla", image: carretilla, price: 585000 },
+        { name: "Overol", image: overol, price: 58000 },
+        { name: "Casco", image: casco, price: 85000 },
+        { name: "Pala", image: pala, price: 75000 },
+        { name: "Pica", image: pica, price: 185000 },
+        { name: "Carretilla", image: carretilla, price: 585000 },
+        { name: "Overol", image: overol, price: 58000 }
     ];
 
     return (
@@ -23,29 +34,40 @@ const LandingPage = () => {
             <div className={classes.landingPageHeader}>
                 <img src={logo} alt="logo" height="150px" />
                 <div className={classes.landingPageTabs}>
-                    <div className={classes.landingPageTabsClick}>Pestaña 1</div>
-                    <div className={classes.landingPageTabsClick}>Pestaña 2</div>
-                    <div className={classes.landingPageTabsClick}>Pestaña 3</div>
-                    <div className={classes.landingPageTabsClick}>Pestaña 4</div>
+                    <div className={classes.landingPageTabsClick}>Inicio</div>
+                    <div className={classes.landingPageTabsClick}>Productos</div>
+                    <div className={classes.landingPageTabsClick}>Misión</div>
+                    <div className={classes.landingPageTabsClick}>Visión</div>
                 </div>
                 <div className={`${classes.landingPageTabs} ${classes.rightTabs}`}>
                     <div className={`${classes.landingPageTabs} ${classes.authTabs}`}>
-                        <div className={classes.landingPageTabsClick}>Opción 1</div>
-                        <div className={classes.landingPageTabsClick}>Opción 2</div>
+                        <div className={classes.landingPageTabsClick}>Contactenos</div>
+                        <div className={classes.landingPageTabsClick}>Carrito de compras</div>
                     </div>
                     <div>lang</div>
                 </div>
             </div>
-            <div className={classes.CardsContainer}>
-                <Each of={products} render={(item, index) =>
-                    <div className={classes.productCard}>
-                        <div>{item.name}</div>
-                        <img src={item.image} alt={item.image} height="180px" />
-                        <div>${item.price} Cop</div>
+            <div className={classes.landingPageBody}>
+                <div className={classes.searchAndCategoriesContainer}>
+                    <div>Search bar</div>
+                    <div>
+                        <div>Todas</div>
+                        <div>Tubería</div>
+                        <div>Cementos</div>
+                        <div>Maquinaria</div>
+                        <div>Dotación</div>
                     </div>
-                } />
+                </div>
+                <div className={classes.CardsContainer}>
+                    <Each of={products} render={(item, index) =>
+                        <div className={classes.productCard}>
+                            <div>{item.name}</div>
+                            <img src={item.image} alt={item.image} height="180px" />
+                            <div>${item.price} Cop</div>
+                        </div>
+                    } />
+                </div>
             </div>
-            <div>Footer</div>
         </div>
     )
 }
