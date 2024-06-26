c14b83345691ebc62ea3010571dc5d4a7150161f
Ruben Ferro <ruben.ferro@aspsols.com>
2024-06-26 11:59:13 -0500

    update week 2 tasks

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './LandingPage.module.scss';
 import { Each } from './Each';
+import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
 import products from '../data/products.json';
 import logo from "../assets/Images/LandingPage/logoMercadoCivil.png";
 import casco from "../assets/Images/constructionProducts/casco.jpeg";
@@ -29,6 +31,14 @@ const LandingPage = () => {
         { name: "Overol", image: overol, price: 58000 }
     ];
 
+    // Para el Search Bar ----------------------------------
+    const [inputValue, setInputValue] = useState('');
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+    };
+    // -----------------------------------------------------
+
     return (
         <div className={classes.LandingPageContainer}>
             <div className={classes.landingPageHeader}>
@@ -40,16 +50,32 @@ const LandingPage = () => {
                     <div className={classes.landingPageTabsClick}>Visión</div>
                 </div>
                 <div className={`${classes.landingPageTabs} ${classes.rightTabs}`}>
-                    <div className={`${classes.landingPageTabs} ${classes.authTabs}`}>
+                    <div className={`${classes.landingPageTabs} ${classes.contactAndShopTabs}`}>
                         <div className={classes.landingPageTabsClick}>Contactenos</div>
-                        <div className={classes.landingPageTabsClick}>Carrito de compras</div>
+                        <Tooltip title="Carrito de compras" placement="bottom" arrow>
+                            <div className={classes.landingPageTabsClick}>
+                                <i className="icon-shopping-cart" ></i>
+                            </div>
+                        </Tooltip>
                     </div>
                     <div>lang</div>
                 </div>
             </div>
             <div className={classes.landingPageBody}>
                 <div className={classes.searchAndCategoriesContainer}>
-                    <div>Search bar</div>
+                    <div className={classes.searchInputContainer} >
+                        <TextField
+                            className={classes.searchInput}
+                            id="outlined-basic"
+                            label="Buscar"
+                            variant="outlined"
+                            value={inputValue}
+                            onChange={handleChange}
+                        />
+                        <div className={classes.xIcon}>
+                            <i className="icon-x" ></i>
+                        </div>
+                    </div>
                     <div>
                         <div>Todas</div>
                         <div>Tubería</div>
