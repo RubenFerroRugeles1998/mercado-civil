d0bb47ed1ca1d03d51daf20fb871f5029a31a2e4
Ruben Ferro <ruben.ferro@aspsols.com>
2024-06-28 17:41:23 -0500

    header update with comments

--- src/components/Screens/HomePage.js
+++ src/components/Screens/HomePage.js
@@ -1,35 +1,33 @@
 import React, { useState } from "react";
 import classes from "./HomePage.module.scss";
-import logo from "../../assets/Images/LandingPage/logoMercadoCivil.png";
+import logo from "../../assets/Images/LandingPage/whiteLogo.jpg";
+import { Each } from '../Utils/Each';
 import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
+import casco from "../../assets/Images/constructionProducts/casco.jpeg";
+import pala from "../../assets/Images/constructionProducts/pala.jpg";
+import pica from "../../assets/Images/constructionProducts/pica.jpeg";
+import carretilla from "../../assets/Images/constructionProducts/carretilla.jpeg";
+import overol from "../../assets/Images/constructionProducts/overol.jpeg";
+
 
 const LandingPage = () => {
 
-    // let products = [
-    //     { name: "Casco", image: casco, price: 85000 },
-    //     { name: "Pala", image: pala, price: 75000 },
-    //     { name: "Pica", image: pica, price: 185000 },
-    //     { name: "Carretilla", image: carretilla, price: 585000 },
-    //     { name: "Overol", image: overol, price: 58000 },
-    //     { name: "Casco", image: casco, price: 85000 },
-    //     { name: "Pala", image: pala, price: 75000 },
-    //     { name: "Pica", image: pica, price: 185000 },
-    //     { name: "Carretilla", image: carretilla, price: 585000 },
-    //     { name: "Overol", image: overol, price: 58000 },
-    //     { name: "Casco", image: casco, price: 85000 },
-    //     { name: "Pala", image: pala, price: 75000 },
-    //     { name: "Pica", image: pica, price: 185000 },
-    //     { name: "Carretilla", image: carretilla, price: 585000 },
-    //     { name: "Overol", image: overol, price: 58000 }
-    // ];
+    let products = [
+        { name: "Casco", image: casco, price: 85000 },
+        { name: "Pala", image: pala, price: 75000 },
+        { name: "Pica", image: pica, price: 185000 },
+        { name: "Carretilla", image: carretilla, price: 585000 },
+        { name: "Overol", image: overol, price: 58000 }
+    ];
 
-    // let categories = [
-    //     { icon: "icon-all", name: "Todas" },
-    //     { icon: "icon-pipe", name: "Tubería" },
-    //     { icon: "icon-concrete", name: "Cementos" },
-    //     { icon: "icon-machinery", name: "Maquinaria" },
-    //     { icon: "icon-coveralls", name: "Dotación" }
-    // ]
+    let categories = [
+        { icon: "icon-all", name: "Todas" },
+        { icon: "icon-pipe", name: "Tubería" },
+        { icon: "icon-concrete", name: "Cementos" },
+        { icon: "icon-machinery", name: "Maquinaria" },
+        { icon: "icon-coveralls", name: "Dotación" }
+    ]
 
     // Para el Search Bar ----------------------------------
     const [inputValue, setInputValue] = useState('');
@@ -65,9 +63,41 @@ const LandingPage = () => {
                         <i className="icon-x" onClick={clearSearchBar}></i>
                     </div>
                 </div>
-                <div>Contactenos</div>
-                <div>Carrito de compras</div>
-                <div>Otro icono</div>
+                <div className={classes.contactUs}>Contactenos</div>
+                <Tooltip title="Carrito de compras" placement="bottom" arrow>
+                    <div className={classes.shoppingCartIcon}>
+                        <i className="icon-shopping-cart" ></i>
+                    </div>
+                </Tooltip>
+                <Tooltip title="Menu" placement="bottom" arrow>
+                    <div className={classes.menu}>
+                        <i className="icon-align-justify" ></i>
+                    </div>
+                </Tooltip>
+            </div>
+            <div className={classes.LandingPageBody}>
+                <div className={classes.selectorContainer}>
+                    <div className={classes.categoriesContainer}>
+                        <div className={classes.categoriesText} >Categorías</div>
+                        <Each of={categories} render={(item, index) =>
+                            <div className={classes.categoriesIconAndText}>
+                                <div className={item.icon}></div>
+                                <div className={classes.selectCategory} >{item.name}</div>
+                            </div>
+                        } />
+                    </div>
+                </div>
+                <div className={classes.productsContainer}>
+                    <div>Productos: </div>
+                    <div className={classes.productsDisplay}>
+                        <Each of={products} render={(item, index) =>
+                            <div className={classes.productCard}>
+                                <img src={item.image} alt={item.image} height="180px" />
+                            </div>
+                            // <div>{index + 1}</div>
+                        } />
+                    </div>
+                </div>
             </div>
         </div>
     )
