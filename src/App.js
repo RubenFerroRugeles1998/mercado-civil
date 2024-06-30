a985d788562da35bedf5febf9e2ef6b7e797e099
Ruben Ferro <ruben.ferro@aspsols.com>
2024-06-30 18:05:37 -0500

    update pending and remove unused components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.scss';
-import LandingPage from './components/LandingPage';
 import HomePage from "./components/Screens/HomePage";
 
 function App() {
   return (
-    // <LandingPage/>
     <HomePage/>
   );
 }
